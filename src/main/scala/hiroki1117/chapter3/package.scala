ad113d42e52a00e8b94538d48ed3a2e6a2bdfe67
hiroki <scala1117@gmail.com>
2019-06-26 00:48:05 +0900

    chapter3

--- src/main/scala/hiroki1117/chapter3/package.scala
+++ src/main/scala/hiroki1117/chapter3/package.scala
@@ -206,4 +206,49 @@ package object chapter3 {
       case Cons(h, t) => Cons(h, take(t, n-1))
     }
   }
+
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  object Tree {
+
+    def apply[A](a: A): Tree[A] = Leaf(a)
+
+    def branch[A](l:Tree[A], r:Tree[A]): Tree[A] = Branch(l,r)
+
+    def size[A](tree: Tree[A]): Int = tree match {
+      case Leaf(_) => 1
+      case Branch(l, r) => size(l) + size(r)
+    }
+
+    def maximum[A: Ordering](tree: Tree[A]): A = tree match {
+      case Leaf(v) => v
+      case Branch(l, r) => implicitly[Ordering[A]].max(maximum(l), maximum(r))
+    }
+
+    def depth[A](tree: Tree[A]): Int = tree match {
+      case Leaf(_) => 0
+      case Branch(l, r) => (1+depth(l)) max (1+depth((r)))
+    }
+
+    def map[A,B](tree: Tree[A])(f: A=>B):Tree[B] = tree match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(l,r) => Branch(map(l)(f), map(r)(f))
+    }
+
+    def fold[A, B](tree: Tree[A])(f: A => B)(op: (B, B) => B): B = tree match {
+      case Leaf(v) => f(v)
+      case Branch(l, r) => op(fold(l)(f)(op), fold(r)(f)(op))
+    }
+
+    def size2[A](tree:Tree[A]): Int = fold(tree)(_ => 1)(_ + _)
+
+    def maximum2[A: Ordering](tree:Tree[A]): A = fold(tree)(identity)(implicitly[Ordering[A]].max(_, _))
+
+    def depth2[A](tree:Tree[A]): Int = fold(tree)(_=>1)(_ max _)
+
+    def map2[A,B](tree:Tree[A])(f:A=>B):Tree[B] = fold(tree)(v=>Tree(f(v)))((l,r)=>Tree.branch(l, r))
+
+  }
 }
