19fbf62439b2eb4f60afcddca1f70919085b0a4c
hiroki <scala1117@gmail.com>
2019-07-03 23:51:55 +0900

    chapter3 option

--- src/main/scala/hiroki1117/chapter4/package.scala
+++ src/main/scala/hiroki1117/chapter4/package.scala
@@ -24,7 +24,19 @@ package object chapter4 {
 
 
   object Option {
-    def apply[A](a: A): Option[A] = Some(a)
-  }
+    def apply[A](a: => A): Option[A] = try{
+      val b = a
+      Some(b)
+    } catch {
+      case Exception => None
+    }
 
+    def mean(xs: Seq[Double]): Option[Double] =
+      if (xs.isEmpty) None else Some(xs.sum / xs.length)
+
+    def variance(xs: Seq[Double]): Option[Double] = {
+      val m: Option[Double] = mean(xs)
+      m.flatMap(ave => mean(xs.map(e => Math.pow(e-ave, 2))))
+    }
+  }
 }
