fa84b8104c31d1e1232b7a876124569640243f10
hiroki <scala1117@gmail.com>
2019-07-08 00:46:34 +0900

    chapter6 rand

--- src/main/scala/hiroki1117/chapter6/package.scala
+++ src/main/scala/hiroki1117/chapter6/package.scala
@@ -54,4 +54,55 @@ package object chapter6 {
       }
       (result.toList, next)
     }
+
+  type Rand[+A] = RNG => (A,RNG)
+
+  val int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A,B](s: Rand[A])(f: A=>B): Rand[B] =
+    rng => {
+      val (a, next) = s(rng)
+      (f(a), next)
+    }
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i=>i-i%2)
+
+  def doubleByMap: Rand[Double] = map(nonNegativeInt)(i => i.toDouble/Int.MaxValue)
+
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A,B)=>C): Rand[C] =
+    rng => {
+      val (a, next) = ra(rng)
+      val (b, next2) = rb(next)
+      (f(a,b), next2)
+    }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra,rb)(_ + _)
+
+  def randIntDouble: Rand[(Int,Double)] = both(int, double)
+
+  def randDouble: Rand[(Double,Int)] = both(double, int)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(Nil): Rand[List[A]]){(e, acc) => map2(e, acc)(_::_)}
+
+  def ints(counts: Int): Rand[List[Int]] = sequence(List.fill(counts)(int))
+
+  def flatMap[A,B](f: Rand[A])(g: A=>Rand[B]): Rand[B] =
+    rng => {
+      val (a, next) = f(rng)
+      g(a)(next)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] = flatMap(nonNegativeInt) { i =>
+    val mod = i%n
+    if(i+(n-1)-mod>=0) unit(mod) else nonNegativeLessThan(n)
+  }
+
+  def mapByFlat[A,B](f: Rand[A])(g:A=>B):Rand[B] = flatMap(f)(x=>unit(g(x)))
+
+  def map2ByFlat[A,B,C](fa:Rand[A], fb:Rand[B])(f:(A,B)=>C):Rand[C] = flatMap(fa)(a => map(fb)(b=>f(a,b)))
+
+  def rollDie: Rand[Int] = map(nonNegativeLessThan(6))(_+1)
 }
